c4d758f275d312412cbf19a6a8ad8d0defe5bbb3
schulze-paul <bl4ckp4nther4@gmail.com>
2022-02-02 22:56:25 +0100

    Adding documentation

--- game_board.py
+++ game_board.py
@@ -2,6 +2,30 @@ import numpy as np
 import copy
 
 class GameBoard(): 
+    """
+    The class game board is a board state of a game of peg solitaire.
+    Attributes:
+    -----------
+    board : list
+        The game board where:
+            -128 represents not a field
+            1 represents a peg and
+            -1 represents a hole.
+    
+    Methods:
+    --------
+    __str__():
+        Generates a visual representation of the board as a string. 
+    get_possible_moves():
+        Computes possible moves in current board state.
+    make_move(move_location : list):
+        Makes the specified move and manipulates the board state.
+    get_new_board_state(move_location : list):
+        Makes the specified move on a copy of the board state.
+    is_perfect():
+        Checks whether or not the current board state is a perfect state
+        (only one peg remaining). 
+    """
 
     def __init__(self):
         self.board = -128*np.ones((7,7))
@@ -13,15 +37,21 @@ class GameBoard():
 
         self.board[3,3] = -1
 
+    def __str__(self) -> str:
+        """
+        Generates a visual representation of the board as a string.
+        
+        Returns:
+        --------
+        str : a visual respresentation of the board state.
+        """
 
-
-    def __str__(self):
         pixels = "\n"
 
         for x_index in range(0,7):
             for y_index in range(0,7):
                 pixel_source = self.board[x_index, y_index]
-                
+
                 if pixel_source == -128:
                     pixels = pixels + "  "
                 if pixel_source == 1:
@@ -29,10 +59,18 @@ class GameBoard():
                 if pixel_source == -1:
                     pixels = pixels + " •"
             pixels = pixels + "\n"
-        
+
         return pixels
 
-    def get_possible_moves(self):
+    def get_possible_moves(self) -> list:
+        """
+        Computes possible moves in current board state.
+        
+        Returns:
+        list : list of possible move locations
+            [jumping peg location, jumped over peg location, jump target hole location]
+        """
+
         move_locations = []
 
         for x_index in range(0,5):
@@ -47,17 +85,44 @@ class GameBoard():
 
         return move_locations
 
-    def make_move(self, move_location):
+    def make_move(self, move_location : list):
+        """
+        Makes the specified move and manipulates the board state.
+        Parameters:
+        -----------
+        move_location : list
+            [jumping peg location, jumped over peg location, jump target hole location].
+        """
+
         self.board[move_location] = -self.board[move_location]
 
+    def get_new_board_state(self, move_location : list) -> GameBoard:
+        """
+        Makes the specified move on a copy of the board state.
+        Parameters:
+        -----------
+        move_location : list
+            [jumping peg location, jumped over peg location, jump target hole location].
+        Returns:
+        --------
+        GameBoard : copy of the board state, modified.
+        """
 
-    def get_new_board_state(self, move_location):
         new_board = copy.deepcopy(self)
         new_board.make_move(move_location)
-        #print(new_board)
+
         return new_board
 
-    def is_perfect(self):
+    def is_perfect(self) -> bool:
+        """
+        Checks whether or not the current board state is a perfect state
+        (only one peg remaining). 
+        
+        Returns:
+        --------
+        bool : True if the board state is perfect.
+        """
+
         perfect_board = -128*np.ones((7,7))
         for x_index in range(2,5):
             for y_index in range(0,7):
--- tree_creator.py
+++ tree_creator.py
@@ -1,11 +1,35 @@
-
 from game_board import GameBoard
 
 game_number = 1
 
 class GameState():
+    """
+    The class GameState represents a state in a game of peg solitaire.
+    It can compute all possible subsequent gamestates as a tree structure.
+    Attributes:
+    -----------
+    move : list
+        The move that lead to the current board state, defined as 
+        [jumping peg location, jumped over peg location, jump target hole location].
+    board_state : GameBoard
+        The current state of the board.
+    previous_game_state : GameBoard
+        The state of the board that preceded
+    possible_moves : list
+        list of possible moves from this board state
+    subsequent_game_states : list
+        list of subsequent game states possible from this game state
+    
+    Methods:
+    --------
+    is_final_game_state()
+        Checks if the number of possible moves is 0.
+    grow_branches()
+        Computes the subsequent game states.
+    """
 
-    def __init__(self, move, board_state, previous_game_state):
+
+    def __init__(self, move : list, board_state : GameBoard, previous_game_state : GameState):
         self.move = move
         self.board_state = board_state
         self.previous_game_state = previous_game_state
@@ -13,14 +37,28 @@ class GameState():
         # calculate possible moves
         self.possible_moves = board_state.get_possible_moves()
 
-    def is_final_game_state(self):
+    def is_final_game_state(self) -> bool:
+        """
+        Checks if the number of possible moves is 0.
+        Returns:
+        --------
+        bool : True if no moves are possible
+        """
         if len(self.possible_moves) > 0:
             return False
         else:
             return True
 
-
     def grow_branches(self):
+        """
+        Computes the subsequent game states.
+        First, the board states and game states of the possible moves are calculated.
+        Then, it checks if there are any new branches after this step.
+        If new branches need to be calculated, then the algorithm recursively computes 
+        the subsequent game states of all subsequent game states.
+        All possible game states are calculated.  
+        """
+
         # create new branches
         self.subsequent_game_states = []
 
@@ -55,4 +93,4 @@ game_board = GameBoard()
 
 game_state = GameState(None, game_board, None)
 
-game_state.grow_branches()
+game_state.grow_branches() 
